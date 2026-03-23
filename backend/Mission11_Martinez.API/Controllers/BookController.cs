e957758abbcc919f158d7d4392a5b63e039b45e8
georgem03 <georgem0@byu.edu>
2026-03-22 18:17:57 -0600

    Database

--- backend/Mission11_Martinez.API/Controllers/BookController.cs
+++ backend/Mission11_Martinez.API/Controllers/BookController.cs
@@ -1,6 +1,23 @@
-namespace Mission11_Martinez.API.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission11_Martinez.API.Data;
 
-public class BookController
+namespace Mission11_Martinez.API.Controllers
 {
-    
+    [Route("[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookStoreDbContext _bookContext;
+        
+        public BookController(BookStoreDbContext temp) => _bookContext = temp;
+        
+        
+        public IEnumerable<Book> GetBooks()
+        {
+            var something = _bookContext.Books.ToList();
+            
+            return something;
+        }
+    }
 }
--- backend/Mission11_Martinez.API/Data/BookStoreDbContext.cs
+++ backend/Mission11_Martinez.API/Data/BookStoreDbContext.cs
@@ -1,6 +1,18 @@
-namespace Mission11_Martinez.API.Data;
+using Microsoft.EntityFrameworkCore;
+namespace Mission11_Martinez.API.Data
 
-public class BookStoreDbContext
 {
-    
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Book> Books { get; set; }
+    }
+
+
 }
+
+
+
